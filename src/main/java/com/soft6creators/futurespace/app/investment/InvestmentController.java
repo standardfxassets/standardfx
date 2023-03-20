436e958de736d879d777f82e445a1f32c59bb92b
standardfxassets <126254424+standardfxassets@users.noreply.github.com>
2023-03-20 20:51:52 +0100

    Update InvestmentController.java

--- src/main/java/com/soft6creators/futurespace/app/investment/InvestmentController.java
+++ src/main/java/com/soft6creators/futurespace/app/investment/InvestmentController.java
@@ -50,7 +50,7 @@ public class InvestmentController {
 		return investmentService.getInvestMents();
 	}
 
-        @RequestMapping("/investment/{investmentId}/delete")
+        @RequestMapping(method = RequestMethod.POST, value = "/investment/{investmentId}/delete")
 	public void deleteInvestment(int investmentId) {
 		investmentService.deleteInvestment(int investmentId);
 	}
